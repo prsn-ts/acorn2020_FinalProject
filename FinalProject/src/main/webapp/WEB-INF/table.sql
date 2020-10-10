5619b2191ca1ee97ee8e2bfa9e141a5b3588e31c
USER <USER@121.133.180.90>
2020-10-10 15:39:18 +0900

    table.sql 정리

--- FinalProject/src/main/webapp/WEB-INF/table.sql
+++ FinalProject/src/main/webapp/WEB-INF/table.sql
@@ -1,7 +1,3 @@
--- 자신이 생성한 테이블과 시퀀스  추가하기
-
-alter table sbproduct add(buycount number);
-
 --주문테이블
 CREATE TABLE sborder
 (
@@ -17,34 +13,14 @@ CREATE TABLE sborder
     sboption       CLOB             NOT NULL, 
     CONSTRAINT SBORDER_PK PRIMARY KEY (num)
 );
-
 --주문테이블 시퀀스
-
 CREATE SEQUENCE sborder_seq;
 
---계좌테이블
-CREATE TABLE sblogin_account
-(
-    id       VARCHAR2(100)    NOT NULL, 
-    money    NUMBER           NOT NULL, 
-    point    NUMBER           NOT NULL
-);
 
-ALTER TABLE sblogin_account
-    ADD CONSTRAINT FK_sblogin_account_id_sblogin_ FOREIGN KEY (id)
-        REFERENCES sblogin (id) on delete cascade;
 
--- 공지 사항 테이블
-CREATE TABLE sbnotice(
-	num NUMBER PRIMARY KEY,
-	writer VARCHAR2(100) NOT NULL,
-	title VARCHAR2(100) NOT NULL,
-	content CLOB,
-	viewCount NUMBER, --조회수
-	regdate DATE
-);
 
-CREATE SEQUENCE sbnotice_seq;
+
+
 
 -- 사용자(회원) 정보를 저장할 테이블
 CREATE TABLE sblogin(
@@ -57,7 +33,44 @@ CREATE TABLE sblogin(
 	trust_consent VARCHAR2(50) NOT NULL, -- 개인정보 처리위탁동의
 	profile VARCHAR2(100), -- 프로필 이미지 경로를 저장할 칼럼
 	regdate DATE -- 가입일 관련
+);
 	
+	
+	
+	
+	
+
+	
+
+
+
+	
+
+
+
+
+-- 공지 사항 테이블
+CREATE TABLE sbnotice(
+	num NUMBER PRIMARY KEY,
+	writer VARCHAR2(100) NOT NULL,
+	title VARCHAR2(100) NOT NULL,
+	content CLOB,
+	viewCount NUMBER, --조회수
+	regdate DATE
+);
+-- 시퀀스 생성
+CREATE SEQUENCE sbnotice_seq;
+
+
+
+
+
+
+
+
+
+
+
 -- 댓글(상품평) 저장하는 테이블
 CREATE TABLE sbproduct_review(
 	num NUMBER PRIMARY KEY, -- 댓글의 글 번호
@@ -72,6 +85,22 @@ CREATE TABLE sbproduct_review(
 -- 댓글(상품평) 관련 시퀀스 
 CREATE SEQUENCE sbproduct_review_seq;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- 상품 상세 정보 테이블
 CREATE TABLE sbproduct
 (
     num            NUMBER            NOT NULL, 
@@ -80,17 +109,33 @@ CREATE TABLE sbproduct
     content        CLOB              NULL, 
     price          NUMBER            NOT NULL, 
     regdate        DATE              NOT NULL, 
-    profile        NVARCHAR2(100)    NULL, 
-    profile2       NVARCHAR2(100)    NULL, 
+    profile        NVARCHAR2(100)    NULL,
+    buycount	   NUMBER			 NULL,
     CONSTRAINT SBPRODUCT_PK PRIMARY KEY (num)
 );
 --시퀀스 생성
 CREATE SEQUENCE sbproduct_seq;
 --productname 에 유니크키 설정
 alter table sbproduct add constraint primarykey22 unique (productname);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 --상품 추가정보 테이블
 CREATE TABLE sbproduct_sub
 (
+	subNum     NUMBER    PRIMARY KEY,
     sbsize     NUMBER    NOT NULL, 
     sbcount    NUMBER    NOT NULL, 
     num        NUMBER    NOT NULL
@@ -99,5 +144,61 @@ CREATE TABLE sbproduct_sub
 ALTER TABLE sbproduct_sub
     ADD CONSTRAINT FK_sbproduct_sub_num_sbproduct FOREIGN KEY (num)
         REFERENCES sbproduct (num) on delete cascade;
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+--계좌테이블
+CREATE TABLE sblogin_account
+(
+    id       VARCHAR2(100)    NOT NULL, 
+    money    NUMBER           NOT NULL, 
+    point    NUMBER           NOT NULL
+);
+ALTER TABLE sblogin_account
+    ADD CONSTRAINT FK_sblogin_account_id_sblogin_ FOREIGN KEY (id)
+        REFERENCES sblogin (id) on delete cascade;
+
+
+
+
+	
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+-- 삭제 관리
+
+DROP TABLE sborder;
+DROP TABLE sbproduct_review;
+DROP TABLE sbproduct_sub;
+DROP TABLE sbnotice;
+DROP TABLE sblogin;
+DROP TABLE sbproduct;
+
+DROP SEQUENCE sborder_seq;
+DROP SEQUENCE sbnotice_seq;
+DROP SEQUENCE sbproduct_review_seq;
+DROP SEQUENCE sbproduct_seq;
+
+
+
+
+
         
 
